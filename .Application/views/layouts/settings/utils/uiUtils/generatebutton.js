ff10dc08c8bef84fd246c97dea937fcfb3a3ba43
x20Hz <98477212+x20Hz@users.noreply.github.com>
2022-01-26 17:34:38 +0000

    Create generatebutton.js

---
+++ .Application/views/layouts/settings/utils/uiUtils/generatebutton.js
@@ -0,0 +1,35 @@
+let eventMappers = new Set();
+
+export const generateButton = (
+  id,
+  label,
+  callback,
+  additionalClasses,
+  toolTip
+) => {
+  if (!eventMappers.has(id)) {
+    initializeListensers(id, callback);
+    eventMappers.add(id);
+  }
+  return `<button title=${
+    toolTip || label
+  } class="btn-standard ${additionalClasses}" id="${id}">${label}</button>`;
+};
+
+const initializeListensers = (id, callback) => {
+  $(document).on(
+    {
+      mouseenter: function () {
+        $(this).addClass("hover");
+      },
+      mouseleave: function () {
+        $(this).removeClass("hover");
+      },
+      click: function () {
+        callback();
+      },
+    },
+    `#${id}`
+  );
+};
+© 2022 GitHub, Inc.
